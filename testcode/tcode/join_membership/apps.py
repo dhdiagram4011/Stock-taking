9b896e5dc096c99d4ba1cfc5cba676e200c6ed0f
golbkim <dhdiagram@gmail.com>
2020-02-23 12:37:41 +0900

    join_member app delete

--- testcode/tcode/join_membership/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JoinMembershipConfig(AppConfig):
-    name = 'join_membership'
--- testcode/tcode/stock_member/admin.py
+++ testcode/tcode/stock_member/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class UserOption(admin.ModelAdmin):
+    list_display = ['username','email','first_name','last_name','depart','position','phone','floor']
+
+admin.site.register(User,UserOption)
